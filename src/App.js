df5c6e1accc3730e0f0b8ac1cae2d720b61e8b5e
Tommy Han <kattyntommy@gmail.com>
2022-11-29 02:44:07 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import './App.css';
-import { Component } from 'react';
+import "./App.css";
+import { Component } from "react";
 
 class App extends Component {
-
   // only necessary for Class
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      monster1: {
-        name: "Linda"
-      },
-      monster2: {
-        name: "Frank"
-      },
-      monster3: {
-        name: "Jackie"
-      }
-
-    }
+      monsters: [
+        {
+          name: "Linda",
+        },
+        {
+          name: "Jackie",
+        },
+        {
+          name: "Makie",
+        },
+      ],
+    };
   }
-  render(){
+  render() {
     return (
       <div className="App">
-        <h1>{this.state.monster1.name}</h1>
-        <h1>{this.state.monster2.name}</h1>
-        <h1>{this.state.monster3.name}</h1>
+        {this.state.monsters.map((monster) => {
+          return <h1>{monster.name}</h1>;
+        })}
       </div>
     );
   }
-
 }
 
 export default App;
