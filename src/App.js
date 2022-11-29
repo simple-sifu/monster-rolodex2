9fc5e60ed43f9ad46e9f91d9aae5fcd32ab33635
Tommy Han <kattyntommy@gmail.com>
2022-11-29 03:42:00 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,24 +6,30 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: "Linda",
-        },
-        {
-          name: "Jackie",
-        },
-        {
-          name: "Makie",
-        },
-      ],
+      monsters: [],
     };
   }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
+      response.json().then((users) => {
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
+      })
+    );
+  }
+
   render() {
     return (
       <div className="App">
         {this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>;
+          return <h1 key={monster.id}>{monster.name}</h1>;
         })}
       </div>
     );
