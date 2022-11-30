440ca09af6213774237a96df29e0f4b11409f772
Tommy Han <kattyntommy@gmail.com>
2022-11-30 21:01:03 +0000

    Update search-box.component.jsx

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,16 +1,14 @@
 // import { Component } from "react";
 import "./search-box.styles.css";
 
-const SearchBox = ({ className, placeholder, onChangeHandler }) => {
-  return (
-    <input
-      className={`search-box ${className}`}
-      type="search"
-      placeholder={placeholder}
-      onChange={onChangeHandler}
-    />
-  );
-};
+const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+  <input
+    className={`search-box ${className}`}
+    type="search"
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
 // class SearchBox extends Component {
 //   render() {
 //     return (
